76d243370ce6dc2fd3b6c828920a115e27a19802
pacio_d <pacio_d@f111.ub.lan>
2024-06-28 10:40:51 +0800

    10:01AM

--- export/account.zip.sql
+++ export/account.zip.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2024 at 08:06 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jun 28, 2024 at 04:40 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
+  `id` int(250) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `FirstName` varchar(50) NOT NULL,
   `LastName` varchar(50) NOT NULL,
-  `ID` int(50) NOT NULL,
   `role` enum('admin','employee') NOT NULL DEFAULT 'employee'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,12 +40,12 @@ CREATE TABLE `account` (
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`email`, `password`, `FirstName`, `LastName`, `ID`, `role`) VALUES
-('asd@gmail.com', '7815696ecbf1c96e6894b779456d330e', 'asd', 'dsa', 1, 'employee'),
-('admin@ph', '202cb962ac59075b964b07152d234b70', 'admin', 'ph', 10, 'admin'),
-('user@ph', '202cb962ac59075b964b07152d234b70', 'user1', 'userv1', 11, 'employee'),
-('user2@ph', '202cb962ac59075b964b07152d234b70', 'user', '2', 12, 'employee'),
-('admin@php', '202cb962ac59075b964b07152d234b70', 'admin', '#__#', 13, 'admin');
+INSERT INTO `account` (`id`, `email`, `password`, `FirstName`, `LastName`, `role`) VALUES
+(2, 'admin@ph', '202cb962ac59075b964b07152d234b70', 'admin', 'ph', 'admin'),
+(3, 'user1@ph', '202cb962ac59075b964b07152d234b70', 'user1', 'userv1', 'employee'),
+(6, 'user6@ph', '202cb962ac59075b964b07152d234b70', 'Lawrence', 'Jimenez', 'employee'),
+(9, 'user3@ph', '202cb962ac59075b964b07152d234b70', 'sample', 'sample', 'employee'),
+(11, 'admin2@ph', '202cb962ac59075b964b07152d234b70', 'Lawrence', 'Jimenez', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -55,7 +55,7 @@ INSERT INTO `account` (`email`, `password`, `FirstName`, `LastName`, `ID`, `role
 -- Indexes for table `account`
 --
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -65,7 +65,7 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
